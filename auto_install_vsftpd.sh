e378d8ea13c64fd18be92f68705621ec6c75bfed
yueyue <2385080188@qq.com>
2020-06-19 11:15:14 +0800

    vsftpd

---
+++ auto_install_vsftpd.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#2020.5.12 10:10:10
+#auto isntall vsftpd 
+#by author lh
+F_YUM="yum install -y"
+F_DIR="/etc/vsftpd"
+F_DB="vsftpd_login"
+F_VIR=""
+$F_YUM vsftpd*
+rpm -qa |grep vsftpd
+systemctl restart vsftpd.service
+
+
+$F_YUM  pam* libdb-utils libdb* --skip-broken
+
+touch $F_DIR/ftpusers.txt
+echo "jfedu001
+123456
+">$F_DIR/ftpusers.txt
+
+db_load -T -t hash -f $F_DIR/ftpusers.txt $F_DIR/${F_DB}.db
+
+chmod 755 $F_DIR/${F_DB}.db
+
+echo "
+auth required pam_userdb.so db=$F_DIR/${F_DB}
+account required pam_userdb.so db=$F_DIR/${F_DB}">/etc/pam.d/vsftpd
+
+useradd -s /sbin/nologin ftpuser
+
+echo "
+pam_service_name=vsftpd
+guest_enable=YES
+guest_username=ftpuser
+user_config_dir=/etc/vsftpd/vsftpd_user_conf
+virtual_use_local_privs=YES
+">>$F_DIR/vsftpd.conf
+
+mkdir -p /etc/${F_USR}/vsftpd_user_conf
+chown -R ftpuser:ftpuser /home/ftpuser
+systemctl stop firewalld.service
+
